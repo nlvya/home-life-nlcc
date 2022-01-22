f633e78b9780327dd42e8145364d080099f555da
Jblack262 <JBlack262@west-mec.org>
2022-01-21 18:29:36 -0700

    heroku initialize

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static('./public'))
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
